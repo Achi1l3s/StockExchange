01da13577b3feeebab66fc789e97225461f68cf8
Nikolai Faist <106889400+Achi1l3s@users.noreply.github.com>
2025-04-11 13:19:54 +0300

    Update ApiService.kt
    KEY hiding

--- app/src/main/java/com/faist/stockexchange/data/ApiService.kt
+++ app/src/main/java/com/faist/stockexchange/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2023-11-30/2024-11-29?adjusted=true&sort=desc&limit=50000&apiKey=lgOXdnS0QOyql17mrWgJO3LcqKGuSpJ8")
+    @GET("aggs/ticker/AAPL/range/1/hour/2023-11-30/2024-11-29?adjusted=true&sort=desc&limit=50000&apiKey=KEY")
     suspend fun loadBars(): Result
 }
